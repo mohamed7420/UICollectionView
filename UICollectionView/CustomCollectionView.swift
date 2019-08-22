c2729dbd0412ec6542c5dbcc4939555d1a877030
mohamedosama2074 <iosengks74@gmail.com>
2019-08-22 15:23:45 +0200

    SimpleUICollectionView

---
+++ UICollectionView/CustomCollectionView.swift
@@ -0,0 +1,16 @@
+//
+//  CustomCollectionView.swift
+//  UICollectionView
+//
+//  Created by Mohamed on 8/22/19.
+//  Copyright © 2019 Mohamed74. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionView: UICollectionViewCell {
+    
+    @IBOutlet weak var imageViewCell: UIImageView!
+    
+    
+}
--- UICollectionView/ViewController.swift
+++ UICollectionView/ViewController.swift
@@ -8,13 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UICollectionViewDelegate , UICollectionViewDataSource{
+  
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    var images:[UIImage] = [UIImage(named: "img1")! , UIImage(named: "img2")! , UIImage(named: "img3")! ,UIImage(named: "img4")! , UIImage(named: "img5")!]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        
     }
 
-
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return images.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as? CustomCollectionView
+        
+        cell?.imageViewCell.image = images[indexPath.row]
+        
+        return cell!
+    }
+    
 }
 
